e773a8670f1b6aa2d82f8813088a6d70b5ff0e0d
Pranitha P <mypranitha08@gmail.com>
2026-06-15 12:29:12 +0530

    Updated Portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+var notetext=document.getElementById("note")
+function note(){
+    notetext.textContent=`Note: This Weather App is currently unavailable online due to API key security restrictions. The project is fully functional in a local development environment.
+`
+}
